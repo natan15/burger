1e04e96c4cc5501482a1ff55f3c8fab058acd8ac
Natan Stuart <nbs2115@gmail.com>
2021-01-03 15:05:40 -0800

    inital install setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ghost Pepper Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Guac Burger', false);
